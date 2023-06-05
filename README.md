103cd78e6bc088468fc94f462f411cd686b90129
Andrey Borisov <royalxxx@yandex.ru>
2023-06-05 20:28:10 +0300

    Add asciinema's recording with application work

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/ef8ba98d297869af9a27/maintainability)](https://codeclimate.com/github/CyberXAndrew/java-project-71/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/ef8ba98d297869af9a27/test_coverage)](https://codeclimate.com/github/CyberXAndrew/java-project-71/test_coverage)
 
+This application compares two json or yaml files and shows the difference in three selectable display formats: stylish, plain and json.
+
 ### Asciinema's program execuiton:
 #### [YAML - default/stylish](https://asciinema.org/a/589167)
 #### [json - default/stylish](https://asciinema.org/a/589166)
 #### [YAML - plain](https://asciinema.org/a/589192)
 #### [json - plain](https://asciinema.org/a/589191)
-#### [Error - different file formats](https://asciinema.org/a/589174)
+#### [YAML - json](https://asciinema.org/a/589776)
+#### [json - json](https://asciinema.org/a/589775)
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -29,20 +29,20 @@ public class Parser {
         for (String key : sortedList) {
             Map<String, Object> map = new LinkedHashMap<>();
             if (map1.containsKey(key) && map2.containsKey(key) && Objects.equals(map1.get(key), map2.get(key))) {
-                map.put("key", key); //пары совпадут целиком
+                map.put("key", key);
                 map.put("oldValue", map1.get(key));
                 map.put("status", "no changes");
             } else if (map1.containsKey(key) && map2.containsKey(key) && !Objects.equals(map1.get(key),
                     map2.get(key))) {
-                map.put("key", key);  //не совпадут по значению
+                map.put("key", key);
                 map.put("oldValue", map1.get(key));
                 map.put("newValue", map2.get(key));
                 map.put("status", "changed");
-            } else if (map1.containsKey(key) && !map2.containsKey(key)) { //если был и исчез
+            } else if (map1.containsKey(key) && !map2.containsKey(key)) {
                 map.put("key", key);
                 map.put("oldValue", map1.get(key));
                 map.put("status", "deleted");
-            } else if (!map1.containsKey(key) && map2.containsKey(key)) { //не был и был добавлен
+            } else if (!map1.containsKey(key) && map2.containsKey(key)) {
                 map.put("key", key);
                 map.put("newValue", map2.get(key));
                 map.put("status", "added");
@@ -55,13 +55,12 @@ public class Parser {
     public static Map<String, Object> getData(String filepath) throws IOException {
         ObjectMapper objectMapper = buildMapper(Differ.getFileFormat(filepath));
         String fileContent = Files.readString(makeAbsolutePath(filepath));
-        //Создана строка данных из содержимого файла по абс пути
         return objectMapper.readValue(fileContent, new TypeReference<>() { });
     }
 
     public static Path makeAbsolutePath(String filepath) {
-        Path filePath = Paths.get(filepath); //создан путь из строки String
-        return filePath.toAbsolutePath().normalize(); //относительный переведен в абсолютный путь
+        Path filePath = Paths.get(filepath);
+        return filePath.toAbsolutePath().normalize();
     }
 
     public static ObjectMapper buildMapper(String format) throws IOException {
